98e53e31aa1199d996fc05961f8b85ce3f136073
kokchun <kokchungiang@gmail.com>
2024-11-19 11:05:26 +0100

    crud operations live demo

---
+++ live_demos/06_crud_operations/sql/alter_statement.sql
@@ -0,0 +1,3 @@
+-- DDL
+ALTER TABLE database.duckdb 
+ADD COLUMN learnt BOOLEAN DEFAULT FALSE;
---
+++ live_demos/06_crud_operations/sql/drop_statement.sql
@@ -0,0 +1,6 @@
+-- DDL 
+SELECT * FROM information_schema.tables;
+
+DROP TABLE database.sql;
+
+DROP SCHEMA programming CASCADE;
