935a5d193752ca0f873b6600ca353c5fc5ac733a
kokchun <kokchungiang@gmail.com>
2024-12-09 10:49:30 +0100

    connect duckdb

---
+++ live_demos/14_python_duckdb/constants.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+# __file__ - dunder file 
+# absolute path to constants.py
+# /"data" -> concatenate with "data"
+# / operator works here because of operator overloading and polymorphism
+DATA_PATH = Path(__file__).parent / "data"
+DATABASE_PATH = Path(__file__).parent / "goteborg_stad.db"
+
+print(DATA_PATH)
